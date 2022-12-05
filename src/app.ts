4463dcf73ae15a374c5911896505b5c4eca6882c
Ricardo Veronezi Ferrão <rvferrao@gmail.com>
2022-12-04 23:29:16 -0300

    DEPLOY - RENDER

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
+import dotenv from 'dotenv';
 import express, {Express} from 'express'
 import cors from "cors"
 
+
+dotenv.config()
 const app: Express = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(3003, () => {
-    console.log("Server Ready 3003!!")
+app.listen(process.env.PORT || 3003, () => {
+    console.log(`Server Ready ${process.env.PORT || 3003} `)
 })
 
 export default app
